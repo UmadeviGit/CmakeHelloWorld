e8d93f838681e2727944987dc6cb959843f49937
UmadeviGit <94173120+UmadeviGit@users.noreply.github.com>
2022-02-24 23:40:25 +0530

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 int main()
 {
-	std::cout << " ......Hello World commit 2..." ;
-		std::cout << " ......Hello World commit 2..." ;
-		std::cout << " ......Hello World commit 2..." ;
-		std::cout << " ......Hello World commit 2..." ;
+	 std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
